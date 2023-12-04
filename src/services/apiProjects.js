5b0a498444c0c85d292261a8ef03458162d16677
Karumaidoi <mainahmwangi12@gmail.com>
2023-12-04 22:39:44 +0300

    fist commit

---
+++ src/services/apiProjects.js
@@ -0,0 +1,57 @@
+import { supabase } from "./supabase";
+
+export async function getProjects() {
+  const { data, error } = await supabase.from("Fertilizer").select();
+
+  if (error) {
+    throw new Error(error?.message);
+  }
+
+  return data;
+}
+
+export async function updateProject(projectId, noBags) {
+  console.log(noBags);
+  const { data, error } = await supabase
+    .from("Fertilizer")
+    .update({ noBags: noBags })
+    .eq("id", projectId);
+
+  if (error) {
+    throw new Error(error?.message);
+  }
+
+  return data;
+}
+
+export async function createBid({ projectId, User: userId, noBags }) {
+  const { data, error } = await supabase.from("Orders").insert({
+    user: userId,
+    isVerified: false,
+  });
+
+  if (error) {
+    throw new Error(error?.message);
+  }
+
+  try {
+    const newProject = await updateProject(projectId, noBags);
+    console.log(newProject);
+  } catch (error) {
+    throw new Error(error?.message);
+  }
+
+  return data;
+}
+
+export async function getBids() {
+  const { data, error } = await supabase
+    .from("Orders")
+    .select("*, Fertilizer(*)");
+
+  if (error) {
+    throw new Error(error?.message);
+  }
+
+  return data;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabaseUrl = "https://xqtuxpfcfishvstxgwre.supabase.co";
+
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxdHV4cGZjZmlzaHZzdHhnd3JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1ODQ0MTIsImV4cCI6MjAxNzE2MDQxMn0.YzFRzmRxoEBmdnrdPsbmp-oZuUFTrqiSnj4eZ-8e65U";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
